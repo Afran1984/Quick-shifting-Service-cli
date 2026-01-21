ced989bad3fead8bdc7cfcc030a9a22d8f579943
Afran1984 <afranrazzak1984@gmail.com>
2026-01-21 22:05:41 +0600

    add Footer & modified Navbar & logo

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router'
 
 const Navbar = () => {
+  const navItems = <>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/booking">Booking</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+        
+  </>
   return (
     <div className="navbar bg-base-100 shadow-sm">
   <div className="navbar-start">
@@ -11,36 +19,23 @@ const Navbar = () => {
       <ul
         tabIndex="-1"
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        {navItems}
       </ul>
     </div>
     <a className="btn btn-ghost text-xl">Quick Shift</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2 bg-base-100 w-40 z-1">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      {navItems}
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <img
+  src="https://i.ibb.co.com/zHZJdW5M/Logo.png"
+  alt="Logo"
+  width={50}
+  height={50}
+/>
   </div>
 </div>
   )
