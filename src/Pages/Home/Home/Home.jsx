7a3096e7ffd05925d7aaf3c5dcbc70150f0fdb11
Afran1984 <afranrazzak1984@gmail.com>
2026-01-26 19:40:20 +0600

    Add Service page &  Details Card

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Bannar from '../Bannar/Bannar.jsx'
 import Card from '../Card/Card.jsx'
+import Service from '../Service/Service.jsx'
 
 const Home = () => {
   return (
     <div>
       <Bannar></Bannar>
       <Card></Card>
+      <Service></Service>
     </div>
   )
 }
---
+++ src/Pages/Home/Service/Service.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { CiDeliveryTruck } from "react-icons/ci";
+
+const Service = () => {
+  return (
+    <div className='text-center mt-15 p-22 bg-cyan-800 rounded-2xl'>
+      <h1 className='font-extrabold text-white'>Our Services</h1>
+      <p className='text-white'>Enjoy fast, reliable parcel delivery with real-time tracking and zero hassle. From personal packages to <br /> business shipments — we deliver on time, every time.</p>
+
+        <div className='grid grid-cols-3 '>
+            <div className='card bg-white shadow-xl m-5 p-5'>
+                <div className='w-20 h-20 mx-auto mb-4 flex items-center justify-center rounded-full bg-blue-100'>
+                    <CiDeliveryTruck className='text-blue-600' size={40}></CiDeliveryTruck>
+                </div>
+                <h2 className='font-extrabold text-lg'>Express  & Standard Delivery</h2>
+                <p className='mt-4'>We deliver parcels within 24–72 hours in Dhaka, Chittagong, Sylhet, Khulna, and Rajshahi. Express delivery available in Dhaka within 4–6 hours from pick-up to drop-off.</p>
+            </div>
+            <div className='card bg-yellow-200 shadow-xl m-5 p-5'>
+                <div className='w-20 h-20 mx-auto mb-4 flex items-center justify-center rounded-full bg-blue-100'>
+                    <CiDeliveryTruck className='text-blue-600' size={40}></CiDeliveryTruck>
+                </div>
+                <h2 className='font-extrabold text-lg'>Nationwide Delivery</h2>
+                <p className='mt-4'>We deliver parcels within 24–72 hours in Dhaka, Chittagong, Sylhet, Khulna, and Rajshahi. Express delivery available in Dhaka within 4–6 hours from pick-up to drop-off.</p>
+            </div>
+            <div className='card bg-white shadow-xl m-5 p-5'>
+                <div className='w-20 h-20 mx-auto mb-4 flex items-center justify-center rounded-full bg-blue-100'>
+                    <CiDeliveryTruck className='text-blue-600' size={40}></CiDeliveryTruck>
+                </div>
+                <h2 className='font-extrabold text-lg'>Express  & Standard Delivery</h2>
+                <p className='mt-4'>We deliver parcels within 24–72 hours in Dhaka, Chittagong, Sylhet, Khulna, and Rajshahi. Express delivery available in Dhaka within 4–6 hours from pick-up to drop-off.</p>
+            </div>
+            <div className='card bg-white shadow-xl m-5 p-5'>
+                <div className='w-20 h-20 mx-auto mb-4 flex items-center justify-center rounded-full bg-blue-100'>
+                    <CiDeliveryTruck className='text-blue-600' size={40}></CiDeliveryTruck>
+                </div>
+                <h2 className='font-extrabold text-lg'>Express  & Standard Delivery</h2>
+                <p className='mt-4'>We deliver parcels within 24–72 hours in Dhaka, Chittagong, Sylhet, Khulna, and Rajshahi. Express delivery available in Dhaka within 4–6 hours from pick-up to drop-off.</p>
+            </div>
+            <div className='card bg-white shadow-xl m-5 p-5'>
+                <div className='w-20 h-20 mx-auto mb-4 flex items-center justify-center rounded-full bg-blue-100'>
+                    <CiDeliveryTruck className='text-blue-600' size={40}></CiDeliveryTruck>
+                </div>
+                <h2 className='font-extrabold text-lg'>Express  & Standard Delivery</h2>
+                <p className='mt-4'>We deliver parcels within 24–72 hours in Dhaka, Chittagong, Sylhet, Khulna, and Rajshahi. Express delivery available in Dhaka within 4–6 hours from pick-up to drop-off.</p>
+            </div>
+            <div className='card bg-white shadow-xl m-5 p-5'>
+                <div className='w-20 h-20 mx-auto mb-4 flex items-center justify-center rounded-full bg-blue-100'>
+                    <CiDeliveryTruck className='text-blue-600' size={40}></CiDeliveryTruck>
+                </div>
+                <h2 className='font-extrabold text-lg'>Express  & Standard Delivery</h2>
+                <p className='mt-4'>We deliver parcels within 24–72 hours in Dhaka, Chittagong, Sylhet, Khulna, and Rajshahi. Express delivery available in Dhaka within 4–6 hours from pick-up to drop-off.</p>
+            </div>
+            
+            
+        </div>
+    </div>
+  )
+}
+
+export default Service
